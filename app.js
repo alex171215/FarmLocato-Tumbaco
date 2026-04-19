a2b0b54c08932d3519ac0c9e276a8f66f78453ca
alex171215 <alexandercarrion918@gmail.com>
2026-04-18 21:17:11 -0500

    quitar limite de tiempo permisos

--- app.js
+++ app.js
@@ -267,12 +267,11 @@ document.addEventListener('DOMContentLoaded', () => {
                     iniciarMapa();
                 },
                 () => {
-                    mostrarAviso("Permiso denegado o no detectado. Usando centro de Tumbaco.");
+                    mostrarAviso("Permiso denegado. Usando centro de Tumbaco.");
                     ubicacionActiva = [...centroTumbaco];
                     iniciarMapa();
                 },
-                // Añadimos un límite de 15 segundos. Si la PC no logra ubicarse, usa Tumbaco y no se congela.
-                { enableHighAccuracy: true, timeout: 15000, maximumAge: 0 }
+                { enableHighAccuracy: true }
             );
         };
     }
